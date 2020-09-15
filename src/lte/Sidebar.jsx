5f9daa21c0c5ef0c2f64b2aa94c6cefcc01b1424
Willian Reis <willianmpreis@gmail.com>
2020-09-15 16:44:49 -0300

    Ajustes mínimos

--- src/lte/Sidebar.jsx
+++ src/lte/Sidebar.jsx
@@ -6,7 +6,7 @@ import SideBarUserPanel from './SidebarUserPanel'
 function Sidebar(props) {
     return (
         <div className="sidebar">
-            <SideBarUserPanel userName="Willian Reis" imagePath="assets/dist/img/user2-160x160.jpg" />
+            <SideBarUserPanel userName="Willian Reis" imagePath="https://avatars2.githubusercontent.com/u/3782288?s=460&u=614c23ac1abcc71b850c08fc6a9a209fab8f2c0a&v=4" />
             <SideBarMenu />
         </div>  
     )
